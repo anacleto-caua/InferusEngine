3368dcba4fdd25f594577bd598b1d0a23aa611ce
Cauã Anacleto <anacleto.for.work@gmail.com>
2025-12-11 13:39:59 -0300

    Fixed project name

--- src/main.cpp
+++ src/main.cpp
@@ -324,7 +324,7 @@ class HelloTriangleApplication {
     void createInstance() {
         VkApplicationInfo appInfo{};
         appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.pApplicationName = "Particle Sim :b";
         appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
         appInfo.pEngineName = "No Engine";
         appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);

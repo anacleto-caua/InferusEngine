edc1788d3a4064ff926333b3815bcd526316ea21
Cauã Anacleto <anacleto.for.work@gmail.com>
2026-01-20 17:26:25 -0300

    Chore: Remove depth test and culling to have an even more default pipeline primitive

--- src/RHI/Pipeline/GraphicsPipelineBuilder.cpp
+++ src/RHI/Pipeline/GraphicsPipelineBuilder.cpp
@@ -34,7 +34,7 @@ GraphicsPipelineBuilder& GraphicsPipelineBuilder::setDefaults() {
     rasterizer.rasterizerDiscardEnable = VK_FALSE;
     rasterizer.polygonMode = VK_POLYGON_MODE_FILL;
     rasterizer.lineWidth = 1.0f;
-    rasterizer.cullMode = VK_CULL_MODE_BACK_BIT;
+    rasterizer.cullMode = VK_CULL_MODE_NONE;
     rasterizer.frontFace = VK_FRONT_FACE_COUNTER_CLOCKWISE;
     rasterizer.depthBiasEnable = VK_FALSE;
     rasterizer.depthBiasConstantFactor = 0.0f;
@@ -50,8 +50,8 @@ GraphicsPipelineBuilder& GraphicsPipelineBuilder::setDefaults() {
     multisampling.minSampleShading = .2f;
 
     depthStencil.sType = VK_STRUCTURE_TYPE_PIPELINE_DEPTH_STENCIL_STATE_CREATE_INFO;
-    depthStencil.depthTestEnable = VK_TRUE;
-    depthStencil.depthWriteEnable = VK_TRUE;
+    depthStencil.depthTestEnable = VK_FALSE;
+    depthStencil.depthWriteEnable = VK_FALSE;
     depthStencil.depthCompareOp = VK_COMPARE_OP_LESS;
     depthStencil.depthBoundsTestEnable = VK_FALSE;
     depthStencil.minDepthBounds = 0.0f;

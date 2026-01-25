36805b8f25e4f2d9139d402884bb378565f90ebc
Cauã Anacleto <anacleto.for.work@gmail.com>
2026-01-25 09:35:50 -0300

    Refactor: Move redundant DescriptorBuilder funcionality inside the Descriptor class

--- src/RHI/Descriptor/Descriptor.cpp
+++ src/RHI/Descriptor/Descriptor.cpp
@@ -1,15 +1,61 @@
 #include "Descriptor.hpp"
 
-#include "DescriptorBuilder.hpp"
-
 void Descriptor::init(VkDevice device, uint32_t binding, VkDescriptorType descriptorType, VkShaderStageFlags stageFlags, VkImageView imageView, VkSampler sampler) {
     this->device = device;
-    DescriptorBuilder::begin(binding, descriptorType, stageFlags)
-        .buildSetTexture(device, imageView, sampler)
-        .fetchObjects(layout, pool, set);
+    setDefaults();
+
+    layoutBinding.binding = binding;
+    layoutBinding.stageFlags = stageFlags;
+    layoutBinding.descriptorType = descriptorType;
+    poolSize.type = descriptorType;
+    descriptorWrite.descriptorType = descriptorType;
+
+    prepareBuild();
+
+    VkDescriptorImageInfo imageInfo{};
+    imageInfo.imageLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
+    imageInfo.imageView = imageView;
+    imageInfo.sampler = sampler;
+    descriptorWrite.pImageInfo = &imageInfo;
+
+    descriptorWrite.dstSet = set;
+    descriptorWrite.dstBinding = layoutBinding.binding;
+    vkUpdateDescriptorSets(device, 1, &descriptorWrite, 0, nullptr);
 }
 
 Descriptor::~Descriptor() {
     if (pool) vkDestroyDescriptorPool(device, pool, nullptr);
     if (layout) vkDestroyDescriptorSetLayout(device, layout, nullptr);
+}
+
+void Descriptor::setDefaults() {
+    layoutBinding.descriptorCount = 1;
+    layoutBinding.pImmutableSamplers = nullptr;
+
+    layoutCreateInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO;
+    layoutCreateInfo.bindingCount = 1;
+
+    poolSize.descriptorCount = 1;
+
+    poolCreateInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO;
+    poolCreateInfo.poolSizeCount = 1;
+    poolCreateInfo.maxSets = 1;
+
+    allocCreateInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_ALLOCATE_INFO;
+    allocCreateInfo.descriptorSetCount = 1;
+
+    descriptorWrite.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
+    descriptorWrite.dstArrayElement = 0;
+    descriptorWrite.descriptorCount = 1;
+
+}
+
+void Descriptor::prepareBuild() {
+    layoutCreateInfo.pBindings = &layoutBinding;
+    vkCreateDescriptorSetLayout(device, &layoutCreateInfo, nullptr, &layout);
+    poolCreateInfo.pPoolSizes = &poolSize;
+    vkCreateDescriptorPool(device, &poolCreateInfo, nullptr, &pool);
+    allocCreateInfo.descriptorPool = pool;
+    allocCreateInfo.pSetLayouts = &layout;
+    vkAllocateDescriptorSets(device, &allocCreateInfo, &set);
 }
--- src/RHI/Descriptor/Descriptor.hpp
+++ src/RHI/Descriptor/Descriptor.hpp
@@ -9,10 +9,18 @@ public:
     VkDescriptorSetLayout layout;
 private:
     VkDevice device;
+    VkDescriptorSetLayoutBinding layoutBinding{};
+    VkDescriptorSetLayoutCreateInfo layoutCreateInfo{};
+    VkDescriptorSetAllocateInfo allocCreateInfo{};
+    VkDescriptorPoolSize poolSize{};
+    VkDescriptorPoolCreateInfo poolCreateInfo{};
+    VkWriteDescriptorSet descriptorWrite{};
 public:
     Descriptor() = default;
     ~Descriptor();
 
     void init(VkDevice device, uint32_t binding, VkDescriptorType descriptorType, VkShaderStageFlags stageFlags, VkImageView imageView, VkSampler sampler);
+    void setDefaults();
+    void prepareBuild();
 private:
 };

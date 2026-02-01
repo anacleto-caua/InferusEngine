1fe5d35785f04a403a7f1285ed301e0ba85aae6e
Cauã Anacleto <anacleto.for.work@gmail.com>
2026-02-01 17:56:27 -0300

    Chore: Create and DOD based abstraction layer for Image creation

---
+++ src/RHI/Image/ImageSystem.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <vector>
+#include <cstdint>
+
+#include <vulkan/vulkan.h>
+#include <vma/vk_mem_alloc.h>
+
+#include "RHI/Image/Image.hpp"
+#include "RHI/Image/ImageCreateDescription.hpp"
+
+class ImageSystem {
+private:
+    VkDevice device;
+    VmaAllocator allocator;
+    std::vector<Image> data;
+    std::vector<uint32_t> freeIndices;
+public:
+    ImageSystem(VkDevice device, VmaAllocator allocator);
+    ~ImageSystem();
+
+    ImageId add(ImageCreateDescription imageCreateDesc);
+    void del(ImageId id);
+    void upload(ImageId id, void *data, size_t size);
+private:
+    void destroy(Image image);
+};

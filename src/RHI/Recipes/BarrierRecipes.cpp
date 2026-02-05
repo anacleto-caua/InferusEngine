65585d67c90028a77641b1090b272a6a212abbff
Cauã Anacleto <anacleto.for.work@gmail.com>
2026-02-05 11:59:10 -0300

    Chore: Add raw version of the default barrier

--- src/RHI/Recipes/BarrierRecipes.cpp
+++ src/RHI/Recipes/BarrierRecipes.cpp
@@ -1,6 +1,27 @@
 #include "BarrierRecipes.hpp"
 
 namespace BarrierRecipes {
+    VkImageMemoryBarrier RawDefault(VkImage image) {
+        VkImageMemoryBarrier barrier {
+            .sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER,
+            .srcAccessMask = 0,
+            .dstAccessMask = 0,
+            .oldLayout = VK_IMAGE_LAYOUT_UNDEFINED,
+            .newLayout = VK_IMAGE_LAYOUT_UNDEFINED,
+            .image = image,
+            .srcQueueFamilyIndex =VK_QUEUE_FAMILY_IGNORED,
+            .dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED,
+            .subresourceRange {
+                .aspectMask = VK_IMAGE_ASPECT_COLOR_BIT,
+                .baseMipLevel = 0,
+                .levelCount = 1,
+                .baseArrayLayer = 0,
+                .layerCount = 1,
+            }
+        };
+        return barrier;
+    }
+
     VkImageMemoryBarrier Default(const Image& image) {
         VkImageMemoryBarrier barrier {
             .sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER,
--- src/RHI/Recipes/BarrierRecipes.hpp
+++ src/RHI/Recipes/BarrierRecipes.hpp
@@ -5,6 +5,7 @@
 #include "RHI/Image/Image.hpp"
 
 namespace BarrierRecipes {
+    VkImageMemoryBarrier RawDefault(VkImage);
     VkImageMemoryBarrier Default(const Image& image);
     VkImageMemoryBarrier TransferDest(const Image& image);
     VkImageMemoryBarrier ShaderRead(const Image& image);
